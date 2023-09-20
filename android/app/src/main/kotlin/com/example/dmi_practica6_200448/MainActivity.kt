3a7e9a576eebf2b6537e037b950915ff52ef63c4
gabi-ctrl <gabriel.guzmangarcia@utxicotepec.edu.mx>
2023-09-20 01:00:26 -0600

    practica6

---
+++ android/app/src/main/kotlin/com/example/dmi_practica6_200448/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dmi_practica6_200448
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:dmi_practica6_200448/src/button.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyButton(),
+    )
+  );
+}
